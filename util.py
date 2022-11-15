f24a5a4c89d894805133e4607711326ba423eac1
aneta-k <kusnierz.aneta@gmail.com>
2022-11-15 14:59:49 +0100

    sorting records on main page

--- util.py
+++ util.py
@@ -1,3 +1,2 @@
 def sort_records(records, order_by, order_direction='desc'):
-    print(order_by)
     return sorted(records, key=lambda d: d[order_by], reverse=True if order_direction=='desc' else False)
