93c99082e8b328030476c0b62790bb81f77c9d03
dapie346 <dawid.pietruszewski@gmail.com>
2022-11-29 21:25:35 +0100

    comment adding time is updated during edition

--- comment_service.py
+++ comment_service.py
@@ -35,6 +35,7 @@ def edit_comment(cursor, comment, message):
     query = """
         UPDATE comment
         SET message = %(message)s,
+        submission_time = NOW(),
         edited_count = edited_count+1
         WHERE id = %(comment_id)s"""
     cursor.execute(query, {'comment_id': comment['id'], 'message': message})
