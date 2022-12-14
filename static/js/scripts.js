609f94332e9aa8503dfb7df4c141c3fd0405c94e
dapie346 <dawid.pietruszewski@gmail.com>
2022-12-14 13:54:31 +0100

    default font stays chosen on the list even when the page is refreshed

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -11,7 +11,7 @@ let ca = document.cookie.split(';');
 for(let i=0; i<ca.length; i++)
   {
   let c = ca[i].trim();
-  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  if (c.indexOf(name)===0) return c.substring(name.length,c.length);
   }
 return "";
 }
@@ -19,9 +19,22 @@ return "";
 function checkFontCookie()
 {
     document.getElementById("top-div").style.fontSize = getCookie("font-size");
+    selectImplementedOption();
 }
 
 let changeFontStyle = function (font) {
     document.getElementById("top-div").style.fontSize = font.value;
     setCookie("font-size", font.value);
+}
+
+function selectImplementedOption() {
+    let optionFromCookies = getCookie("font-size");
+    let listToSelectFrom = document.getElementById('input-font');
+
+    for(let option, option_index = 0; option = listToSelectFrom.options[option_index]; option_index++) {
+        if(option.value === optionFromCookies) {
+            listToSelectFrom.selectedIndex = option_index;
+            break;
+        }
+    }
 }
