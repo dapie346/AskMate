174bafabdd574b00a690fea72401173e0dc0ceac
dapie346 <dawid.pietruszewski@gmail.com>
2022-12-14 14:01:14 +0100

    minor formatting

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,23 +1,20 @@
 checkFontCookie()
-function setCookie(cname,cvalue)
-{
+function setCookie(cname,cvalue) {
     document.cookie = cname+"="+cvalue+";path=/";
 }
 
-function getCookie(cname)
-{
-let name = cname + "=";
-let ca = document.cookie.split(';');
-for(let i=0; i<ca.length; i++)
-  {
-  let c = ca[i].trim();
-  if (c.indexOf(name)===0) return c.substring(name.length,c.length);
-  }
-return "";
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i=0; i<ca.length; i++)
+      {
+      let c = ca[i].trim();
+      if (c.indexOf(name)===0) return c.substring(name.length,c.length);
+      }
+    return "";
 }
 
-function checkFontCookie()
-{
+function checkFontCookie() {
     document.getElementById("top-div").style.fontSize = getCookie("font-size");
     selectImplementedOption();
 }
